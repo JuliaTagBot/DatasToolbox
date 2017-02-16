99248956d834e616a9d1f6344886fde5e600c89e
Michael <savastio@gmail.com>
2017-02-16 14:28:05 -0500

    Added functions for breaking up training matrices for multiple estimators.

--- src/utils.jl
+++ src/utils.jl
@@ -195,3 +195,159 @@ end
 export @infotime
 
 
+#=========================================================================================
+This section is all for breaking a matrix into submatrices based on a class label.
+=========================================================================================#
+getDefaultComparator{T}(::Type{T}) = (==)
+getDefaultComparator{T<:AbstractFloat}(::Type{T}) = (≈)
+export getDefaultComparator
+
+
+"""
+    findboundaries(X[, ncol; comparator=DEFAULT, check_sort=true])
+
+Finds the boundaries between distinct values in the sorted array `X`.  For example, if
+`X = [1, 1, 1, 2, 2, 3, 4]`, `boundaries(X) == [1, 4, 6, 7]`.  This algorithm requires
+the input array to be sorted in the column in which boundaries are determined.
+Do `sort_array=false` in cases where the input array is already sorted
+
+This algorithm only works properly if the input array is already sorted.  By default,
+this function checks if the input array is sorted.  To skip this check, set 
+`check_sort=false`.
+"""
+function findBoundaries{T}(X::Vector{T}; check_sort::Bool=true,
+                           comparator=getDefaultComparator(T))
+    if check_sort && !issorted(X)
+        throw(ArgumentError("Trying to find boundaries on unsorted array."))
+    end
+    boundaries = Int64[1]
+    for i ∈ 2:length(X)
+        !comparator(X[i-1], X[i]) && push!(boundaries, i)
+    end
+    boundaries
+end
+
+function findBoundaries{T}(X::Matrix{T}, ncol::Integer; check_sort::Bool=true,
+                           comparator=getDefaultComparator(T))
+    findBoundaries(X[:,ncol], check_sort=check_sort, comparator=comparator)
+end
+
+
+"""
+    findboundaries!(v[; comparator=DEFAULT])
+
+This is the same as `findboundaries` except that it sorts the vector `v` first. 
+See documentation for `findboundaries`.
+"""
+function findBoundaries!{T}(X::Vector{T}; comparator=getDefaultComparator(T))
+    if sort_array
+        X = sort!(X)
+    end
+    findBoundaries(X, comparator=comparator)
+end
+
+
+"""
+    boundaryClassValues(X[, ncol,] boundaries)
+
+Given `boundaries` between distinct values in column `ncol` of `Matrix` `X` (can also be a
+`Vector`) (see documentation for `findBoundaries`), find the values of the distinct values.
+
+This is essentially the same functionality as unique, except that it is faster since
+boundaries have already been determined.
+"""
+function boundaryClassValues{T}(X::Matrix{T}, ncol::Integer, boundaries::Vector)
+    [X[boundaries[i], ncol] for i ∈ 1:length(boundaries)]
+end
+function boundaryClassValues{T}(v::Vector{T}, boundaries::Vector)
+    [X[boundaries[i]] for i ∈ 1:length(boundaries)]
+end
+
+
+"""
+    findBoundariesVlaues(X[, ncol; check_sort=true, comparator=DEFAULT])
+
+Returns `boundaries, classes` where `boundaries` is the output of `findBoundaries` and
+`classes` is the output of `boundaryClassValues`.  Note that `X` should be sorted before
+inputting it into this function.
+"""
+function findBoundariesValues{T}(X::Vector{T}; check_sort::Bool=true,
+                                 comparator=getDefaultComparator(T))
+    boundaries = findBoundaries(X, check_sort=check_sort, comparator=comparator)
+    classes = boundaryClassValues(X, boundaries)
+    boundaries, classes
+end
+
+function findBoundariesValues{T}(X::Matrix{T}, ncol::Integer; check_sort::Bool=true,
+                                 comparator=getDefaultComparator(T))
+    boundaries = findBoundaries(X, ncol, check_sort=check_sort, comparator=comparator)
+    classes = boundaryClassValues(X, ncol, boundaries)
+    boundaries, classes
+end
+
+
+"""
+    findBoundaryDict(X[, ncol; check_sort=true, comparator=DEFAULT])
+
+For a sorted array `X`, find the boundaries between distinct values of column number
+`ncol`.  
+"""
+function findBoundaryDict{T}(X::Vector{T}; check_sort::Bool=true,
+                             comparator=getDefaultComparator(T))
+    boundaries, classes = findBoundariesValues(X, check_sort=check_sort,
+                                               comparator=comparator)
+    dict = Dict{T,Tuple{Int64,Int64}}();  sizehint!(dict, length(boundaries))
+    for i ∈ 1:length(boundaries)
+        dict[classes[i]] = (boundaries[i], get(boundaries, i+1, length(X)+1)-1)
+    end
+    dict
+end
+
+function findBoundaryDict{T}(X::Matrix{T}, ncol::Integer; check_sort::Bool=true,
+                             comparator=getDefaultComparator(T))
+    boundaries, classes = findBoundariesValues(X, ncol, check_sort=check_sort,
+                                               comparator=comparator)
+    dict = Dict{T,Tuple{Int64,Int64}}();  sizehint!(dict, length(boundaries))
+    for i ∈ 1:length(boundaries)
+        dict[classes[i]] = (boundaries[i], get(boundaries, i+1, size(X,1)+1)-1)
+    end
+    dict
+end
+export findBoundaryDict
+
+
+_order_y(y::Vector, order::Vector) = y[order]
+_order_y(y::Matrix, order::Vector) = y[order, :]
+
+_get_y_interval(y::Vector, r::UnitRange) = y[r]
+_get_y_interval(y::Matrix, r::UnitRange) = y[r, :]
+
+
+"""
+    subMatricesByClass(X, y, ncol)
+
+Breaks the arrays `X` and `y` into dictionaries of `class=>submatrix` pairs where `class`
+is one of the distinct values of the column `ncol` of `X` and `submatrix` is the range
+of rows of `X` or `y` where `X[:,ncol]` has the value `class`.
+
+This function is intended for breaking up training and test sets to be used with sets of
+different models.
+"""
+function subMatricesByClass{T,U}(X::Matrix{T}, y::U, ncol::Integer)
+    if size(X,1) ≠ size(y,1)
+        throw(ArgumentError("X and y must have same number of rows."))
+    end
+    order = sortperm(X[:, ncol])
+    X = X[order, :]
+    y = _order_y(y, order)
+    bdict = findBoundaryDict(X, ncol, check_sort=false)
+    Xdict = Dict{T,Matrix{T}}();  sizehint!(Xdict, length(bdict))
+    ydict = Dict{T,U}();  sizehint!(ydict, length(bdict))
+    for (k, v) ∈ bdict
+        Xdict[k] = X[v[1]:v[2], :]
+        ydict[k] = _get_y_interval(y, v[1]:v[2])
+    end
+    Xdict, ydict
+end
+export subMatricesByClass
+
